a98272dacca71d656849fa754ec26846b55f9e78
kisa002 <vnycall74@naver.com>
2021-12-10 20:28:42 +0900

    [Week3-1] Jetpack Compose Theming

---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Shape.kt
@@ -0,0 +1,15 @@
+package com.codelab.theming.ui.start
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by GwangMoo You on 2021/12/10.
+ */
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
